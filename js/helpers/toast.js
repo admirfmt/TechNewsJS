390c8fb02b83b5d3dccb464b40d5eb7795d3ee84
admirfmt <admir.fmt@gmail.com>
2026-05-16 23:27:59 +0200

    [feat] move Toast to Javascript side

--- js/helpers/toast.js
+++ js/helpers/toast.js
@@ -1,6 +1,17 @@
+function createToastContainer() {
+    let container = document.getElementById('toast-container');
+    if (!container) {
+        container = document.createElement('div');
+        container.id = 'toast-container';
+        container.className = 'fixed z-[999] flex flex-col gap-2.5 right-6 bottom-6';
+        document.body.appendChild(container);
+    }
+    return container;
+}
+
 export function showToast(message, type) {
     if (!type) type = 'success';
-    const container = document.getElementById('toast-container');
+    const container = createToastContainer();
     const toast = document.createElement('div');
     toast.className = 'toast ' + type;
     toast.textContent = message;
